c6eefc36a1bfa98a214820997f561237006d1400
DevScriptTi <00.dev.00.dev.00@gmail.com>
2025-06-04 21:34:26 +0100

    1

--- src/app/[locale]/(Guest)/layout.tsx
+++ src/app/[locale]/(Guest)/layout.tsx
@@ -1,9 +1,7 @@
-import UpBar from "@/lib/ui/components/local/Welcom page/UpBar";
 
-export default function layout({children}: {children : React.ReactNode}) {
+export default function layout({ children }: { children: React.ReactNode }) {
     return (
         <>
-            <UpBar />
             {children}
         </>
     )
